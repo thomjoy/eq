66b1c10fe26dd8cce74d9c999666c7c209789381
Thom Joy <thomjoy1984@gmail.com>
2013-06-12 08:33:09 +1000

    restructure

---
+++ client/app.js
@@ -0,0 +1,11 @@
+// app.js
+requirejs.config({
+    "baseUrl": "client/",
+    "paths": {
+        "underscore":               "lib/underscore", 
+        "backbone":                 "lib/backbone",
+        "backbone.localStorage":    "lib/backbone.localstorage.js", 
+        "jquery":                   "lib/jquery-2.0.2-min",
+
+    }
+});
