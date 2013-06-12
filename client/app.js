fc7448ce031361041b0497aa122c1ff005c2e928
Thom Joy <thomjoy1984@gmail.com>
2013-06-12 13:45:34 +1000

    require async, loading gmaps

--- client/app.js
+++ client/app.js
@@ -1,6 +1,21 @@
 requirejs.config({
     waitSeconds: 30,
     paths: {
+
+        // plugins
+        //create alias to plugins (not needed if plugins are on the baseUrl)
+        async: '/client/lib/requirejs-plugins/src/async',
+        font: '/client/lib/requirejs-plugins/src/font',
+        goog: '/client/lib/requirejs-plugins/src/goog',
+        image: '/client/lib/requirejs-plugins/src/image',
+        json: '/client/lib/requirejs-plugins/src/json',
+        noext: '/client/lib/requirejs-plugins/src/noext',
+        mdown: '/client/lib/requirejs-plugins/src/mdown',
+        propertyParser : '/client/lib/requirejs-plugins/src/propertyParser',
+        markdownConverter : '/client/lib/requirejs-plugins/src/Markdown.Converter',
+        text: '/client/lib/requirejs-plugins/src/text',
+        domReady: '/client/lib/requirejs-plugins/src/domReady(callback)',
+
         underscore: '/client/lib/underscore',
         backbone:   '/client/lib/backbone',
         'backbone.localstorage': '/client/lib/backbone.localstorage',
@@ -9,9 +24,6 @@ requirejs.config({
     shim: {
         underscore: {
             exports: '_'
-        },
-        jquery: {
-            exports: '$'
         },
         backbone: {
             deps: ['underscore', 'jquery'],
--- client/main.js
+++ client/main.js
@@ -1,9 +1,25 @@
+
+// Google Maps
+define(
+    'gmaps',
+    ['async!http://maps.google.com/maps/api/js?v=3&sensor=false'],
+    function(){
+        return window.google.maps;
+    });
+
 // app.js
 define([
+    'gmaps',
     'backbone',
     'jquery'
 ], function(
+    gmaps,
     Backbone,
     $
 ){
+    console.log(gmaps);
+    console.log(Backbone);
+    console.log($);
+
+    
 });
