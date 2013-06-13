b57e012a409c492bc71a5424f416c3fbd367a860
Thom Joy <thomjoy1984@gmail.com>
2013-06-13 15:27:37 +1000

    tidy

--- client/v/listview.js
+++ client/v/listview.js
@@ -5,21 +5,25 @@ define(['backbone'], function() {
         initialize: function() {
             _.extend(this, this.options);
             this.render();
-            this.listItemsAsString = '';
+
+            // re-render the list when the collection changes
             this.listenTo(this.collection, 'reset', this.render);
         },
 
         render: function() {
-            console.log('Collection Changed');
+            // detach from the DOM
             this.$el.detach();
+
+            // remove old nodes
             this.$el.empty();
+
+            // insert the new nodes
             this.$el
-                .html(this.renderListString(this.collection.models))
+                .html(this.renderListAsString(this.collection.models))
                 .appendTo('body');
         },
 
-
-        renderListString: function(list) {
+        renderListAsString: function(list) {
             var tmp = '';
             _.each(list, function(p) {
                 tmp += '<li id="list-item-' + p.attributes.properties.id + '"><strong class="mag ' + p.get('magRating') + '">' + p.attributes.properties.mag + '</span> ' + p.attributes.properties.place  + '</li>';
