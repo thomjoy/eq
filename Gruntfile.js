0561ed8a4cb88925c04e09b3f0febfc0dc721908
Thom Joy <thomjoy1984@gmail.com>
2013-06-18 21:05:44 +1000

    grunt, node_modules

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      all: {
+        options: {
+          curly: true,
+          eqeqeq: true,
+          sub: true,
+          undef: true,
+          boss: true,
+          eqnull: true,
+          browser: true,
+          devel: true,
+          globals: {
+            _: true,
+            $: true,
+            jQuery: true,
+            Backbone: true,
+            require: true,
+            define: true,
+            requirejs: true,
+            describe: true,
+            expect: true,
+            it: true
+          }
+        },
+        files: {
+          src: ['client/app.js', 'client/main.js']
+        } //'client/**/*.js's
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Default task.
+  grunt.registerTask('default', 'jshint');
+
+};
