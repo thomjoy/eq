e6426c5a91fd3d2eb3b30708274937e9fe37732d
Thom Joy <thomjoy1984@gmail.com>
2013-06-18 23:07:31 +1000

    add new grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,8 +34,10 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   // Default task.
   grunt.registerTask('default', 'jshint');
